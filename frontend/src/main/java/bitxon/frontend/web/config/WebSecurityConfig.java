897bd9dc721db239de6a5367e22f59930563adbe
Nikita Karpuk <bitxon.dev@gmail.com>
2021-09-24 16:23:18 -0400

    Logout (TODO logout in docekr compose failed due to iss validation)

--- frontend/src/main/java/bitxon/frontend/web/config/WebSecurityConfig.java
+++ frontend/src/main/java/bitxon/frontend/web/config/WebSecurityConfig.java
@@ -2,10 +2,12 @@ package bitxon.frontend.web.config;
 
 import bitxon.frontend.web.config.extention.CustomGrantedAuthoritiesMapper;
 import bitxon.frontend.web.config.extention.CustomOidcUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
@@ -14,6 +16,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    ClientRegistrationRepository regRepo;
+
+    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        var successHandler = new OidcClientInitiatedLogoutSuccessHandler(regRepo);
+        successHandler.setPostLogoutRedirectUri("{baseUrl}");
+        return successHandler;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //To debug hasAuthority/hasRole see SecurityExpressionRoot
@@ -21,8 +32,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/").permitAll()
             .antMatchers("/actuator/**").permitAll()
             .anyRequest().authenticated()
-            .and()
-            .oauth2Login()
+            .and().logout()
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
+            .and().oauth2Login()
                 .userInfoEndpoint()
                     .userAuthoritiesMapper(new CustomGrantedAuthoritiesMapper())
                     .oidcUserService(new CustomOidcUserService());
--- frontend/src/test/java/bitxon/frontend/web/WebUiApplicationTests.java
+++ frontend/src/test/java/bitxon/frontend/web/WebUiApplicationTests.java
@@ -2,7 +2,9 @@ package bitxon.frontend.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("context-test")
 @SpringBootTest
 class WebUiApplicationTests {
 
